e968e1398e9c400bc4c8990255783182d11cd3e2
ant5555 <ymo6886@gmail.com>
2026-02-17 12:11:00 +0900

    feat: MainViewModel 구현

---
+++ app/src/main/kotlin/com/ant/project/MainVIewModel.kt
@@ -0,0 +1,24 @@
+package com.ant.project
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlin.random.Random
+
+class MainVIewModel: ViewModel() {
+
+    private val _color = MutableStateFlow(0xFFFFFFFF)
+    val color = _color.asStateFlow()
+
+    var composeColor by mutableStateOf(0xFFFFFFFF)
+        private set
+
+    fun generateNewColor() {
+        val color = Random.nextLong(0xFFFFFFFF)
+        _color.value = color
+        composeColor = color
+    }
+}
