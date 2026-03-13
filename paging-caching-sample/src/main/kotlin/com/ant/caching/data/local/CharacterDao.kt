791778e1db1a4951fb34995046a1f73ce5605104
ant5555 <ymo6886@gmail.com>
2026-03-13 17:40:17 +0900

    feat: Database, Dao 구현

---
+++ paging-caching-sample/src/main/kotlin/com/ant/caching/data/local/CharacterDao.kt
@@ -0,0 +1,20 @@
+package com.ant.caching.data.local
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface CharacterDao {
+
+    @Upsert
+    suspend fun upsertAll(characters: List<CharacterEntity>)
+
+    @Query("SELECT * FROM characterentity")
+    fun pagingSource(): PagingSource<Int, CharacterEntity>
+
+    @Query("DELETE FROM characterentity")
+    suspend fun clearAll()
+
+}
---
+++ paging-caching-sample/src/main/kotlin/com/ant/caching/data/local/CharacterDatabase.kt
@@ -0,0 +1,13 @@
+package com.ant.caching.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [CharacterEntity::class],
+    version = 1
+)
+
+abstract class CharacterDatabase: RoomDatabase() {
+    abstract val dao: CharacterDao
+}
