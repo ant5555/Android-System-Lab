63c429ac5f21665839237070b9f54dab4fe94a87
ant5555 <ymo6886@gmail.com>
2026-03-14 09:28:12 +0900

    feat: di 주입

---
+++ paging-caching-sample/src/main/kotlin/com/ant/caching/CharacterApp.kt
@@ -0,0 +1,7 @@
+package com.ant.caching
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CharacterApp: Application()
---
+++ paging-caching-sample/src/main/kotlin/com/ant/caching/di/AppModule.kt
@@ -0,0 +1,61 @@
+package com.ant.caching.di
+
+import android.content.Context
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.room.Room
+import com.ant.caching.data.local.CharacterDatabase
+import com.ant.caching.data.local.CharacterEntity
+import com.ant.caching.data.remote.CharacterApi
+import com.ant.caching.data.remote.CharacterRemoteMediator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideCharacterDatabase(@ApplicationContext context: Context): CharacterDatabase {
+        return Room.databaseBuilder(
+            context,
+            CharacterDatabase::class.java,
+            "characters.db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCharacterApi(): CharacterApi {
+        return Retrofit.Builder()
+            .baseUrl(CharacterApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+
+    @OptIn(ExperimentalPagingApi::class)
+    @Provides
+    @Singleton
+    fun provideCharacterPager(characterDb: CharacterDatabase, characterApi: CharacterApi): Pager<Int, CharacterEntity> {
+        return Pager(
+            config = PagingConfig(pageSize = 20),
+            remoteMediator = CharacterRemoteMediator(
+                characterDb = characterDb,
+                characterApi = characterApi
+            ),
+            pagingSourceFactory = {
+                characterDb.dao.pagingSource()
+            }
+        )
+    }
+}
