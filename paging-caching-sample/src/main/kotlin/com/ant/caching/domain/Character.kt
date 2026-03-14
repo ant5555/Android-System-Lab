923a8d0743d0cb192097dbcef86a3b885aacba63
ant5555 <ymo6886@gmail.com>
2026-03-14 09:08:31 +0900

    feat: 샘플데이터 추가

--- paging-caching-sample/src/main/kotlin/com/ant/caching/domain/Character.kt
+++ paging-caching-sample/src/main/kotlin/com/ant/caching/domain/Character.kt
@@ -6,4 +6,22 @@ data class Character(
     val species: String,
     val status: String,
     val image: String,
-)
+) {
+    companion object {
+        val sample = Character(
+            id = 1,
+            name = "Rick Sanchez",
+            species = "Human",
+            status = "Alive",
+            image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        )
+
+        val sampleList = listOf(
+            sample,
+            sample.copy(id = 2, name = "Morty Smith"),
+            sample.copy(id = 3, name = "Summer Smith"),
+            sample.copy(id = 4, name = "Beth Smith", status = "Dead"),
+            sample.copy(id = 5, name = "Jerry Smith")
+        )
+    }
+}
