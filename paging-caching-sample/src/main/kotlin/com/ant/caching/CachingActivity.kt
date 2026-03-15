f970f49a50c626df1a3a8e3f0a73c169a61a812e
ant5555 <ymo6886@gmail.com>
2026-03-16 06:53:50 +0900

    fix: AndroidEntryPoint 추가

--- paging-caching-sample/src/main/kotlin/com/ant/caching/CachingActivity.kt
+++ paging-caching-sample/src/main/kotlin/com/ant/caching/CachingActivity.kt
@@ -5,18 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.ant.caching.presentation.CharacterScreen
 import com.ant.caching.presentation.CharacterViewModel
 import com.ant.caching.ui.theme.AndroidSystemLabTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CachingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
