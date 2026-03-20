b409870f2ab5308c25cc4591e2d823dc5ba81500
ant5555 <ymo6886@gmail.com>
2026-03-20 12:31:05 +0900

    build: coroutines 의존성 추가

--- coroutine-sample/build.gradle.kts
+++ coroutine-sample/build.gradle.kts
@@ -44,6 +44,9 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
