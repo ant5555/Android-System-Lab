9e4a7e5a0476a4e9f254d8bae8b456a00b5c5e89
ant5555 <ymo6886@gmail.com>
2026-03-24 11:53:21 +0900

    feat: 코루틴 순차처리

--- coroutine-basic-sample/src/main/kotlin/com/ant/coroutine/basic/Main.kt
+++ coroutine-basic-sample/src/main/kotlin/com/ant/coroutine/basic/Main.kt
@@ -3,25 +3,15 @@ package com.ant.coroutine.basic
 import kotlinx.coroutines.*
 
 fun main() = runBlocking<Unit> {
-    // Default — CPU 연산에 최적화, 코어 수만큼 스레드 풀
-    launch(Dispatchers.Default) {
-        println("[${Thread.currentThread().name}] Dispatchers.Default")
+    val updateToken: Job = launch(Dispatchers.IO) {
+        println("[${Thread.currentThread().name}] 토큰 업데이트 시작")
+        delay(100L)
+        println("[${Thread.currentThread().name}] 토큰 업데이트 완료")
     }
 
-    // IO — 파일/네트워크 I/O에 최적화, 스레드 많이 만들 수 있음
-    launch(Dispatchers.IO) {
-        println("[${Thread.currentThread().name}] Dispatchers.IO")
-    }
+    updateToken.join()
 
-    // Unconfined — 처음엔 호출한 스레드, delay 이후엔 재개되는 스레드
-    launch(Dispatchers.Unconfined) {
-        println("[${Thread.currentThread().name}] Dispatchers.Unconfined - delay 전")
-        delay(100)
-        println("[${Thread.currentThread().name}] Dispatchers.Unconfined - delay 후")
-    }
-
-    // Dispatcher 없음 — 부모 스코프(runBlocking) 스레드 그대로 사용
-    launch {
-        println("[${Thread.currentThread().name}] Dispatcher 없음")
+    val networkCallJob: Job = launch(Dispatchers.IO) {
+        println("[${Thread.currentThread().name}] 네트워크 요청")
     }
 }
