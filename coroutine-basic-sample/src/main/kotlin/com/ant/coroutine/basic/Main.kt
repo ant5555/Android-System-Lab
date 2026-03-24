585c6922e163578635cbe547018443265bd945b3
ant5555 <ymo6886@gmail.com>
2026-03-24 11:54:24 +0900

    feat: 코루틴 작업 추가 후 순차처리

--- coroutine-basic-sample/src/main/kotlin/com/ant/coroutine/basic/Main.kt
+++ coroutine-basic-sample/src/main/kotlin/com/ant/coroutine/basic/Main.kt
@@ -9,6 +9,10 @@ fun main() = runBlocking<Unit> {
         println("[${Thread.currentThread().name}] 토큰 업데이트 완료")
     }
 
+    val independentJob: Job = launch(Dispatchers.IO) {
+        println("[${Thread.currentThread().name}] 독립된 작업 시작")
+    }
+
     updateToken.join()
 
     val networkCallJob: Job = launch(Dispatchers.IO) {
