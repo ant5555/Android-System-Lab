bba652528ea0e2aa823dc9070605aec61b02f33c
ant5555 <ymo6886@gmail.com>
2026-04-01 19:13:12 +0900

    feat: 임의로 데이터 스트림 만들어서 디버깅

--- flow-basic-sample/src/main/kotlin/com/ant/flow/basic/Main.kt
+++ flow-basic-sample/src/main/kotlin/com/ant/flow/basic/Main.kt
@@ -1,4 +1,23 @@
 package com.ant.flow.basic
 
-class Main {
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+
+    val flow = flow {
+        emit(1)
+        emit(2)
+        emit(3)
+    }
+    flow.collect { println("collect: $it") }
+
+    listOf("a", "b", "c").asFlow()
+        .collect { println("asFlow: $it") }
+
+    (1..5).asFlow()
+        .collect { println("rangeFlow: $it") }
+
+    flowOf(10, 20, 30)
+        .collect { println("flowOf: $it") }
 }
